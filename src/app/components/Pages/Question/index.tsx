fedfeb58bc007273ef4e8f3e39333b965848ea0f
Erick Oda Coulter <erickodac@hotmail.com>
2024-08-23 10:34:44 -0300

    fix: removed more unused files

--- src/app/components/Pages/Question/index.tsx
+++ src/app/components/Pages/Question/index.tsx
@@ -1,10 +1,8 @@
-import Root from "./Components/Root";
 import StartStimulus from "./Components/StartStimulus";
 import GuessTheTimeSpent from "./Components/GuessTheTimeSpent";
 import GenericQuestion from "./Components/GenericQuestion";
 
 const Question = {
-  Root: Root,
   GenericQuestion: GenericQuestion,
   StartStimulus: StartStimulus,
   GuessTheTimeSpent: GuessTheTimeSpent,
