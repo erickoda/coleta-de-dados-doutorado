9157ac76d87508e700b9afb45623b3ee194edc72
Erick Oda Coulter <erickodac@hotmail.com>
2024-09-04 14:36:30 -0300

    fix: removed debug button

--- src/app/components/Pages/Greet/index.tsx
+++ src/app/components/Pages/Greet/index.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import Title from "../../Global/Title";
-import { usePages } from "@/app/context/pages";
-import { Button } from "@mui/material";
 
 const Greet = () => {
-  const { go_to_previous_page } = usePages();
-
   return (
     <>
       <Title>
         Mais uma vez muito obrigada pela sua participação, a ciência agradece!
       </Title>
-
-      <Button onClick={() => go_to_previous_page()}>Aqui</Button>
     </>
   );
 };
