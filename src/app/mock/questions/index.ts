6139a211452b67e1f95f14aaa9a65bfabf60ab14
Erick Oda <erickodac@hotmail.com>
2024-09-20 17:05:15 -0300

    refactor: refactored mocked questions content

--- src/app/mock/questions/index.ts
+++ src/app/mock/questions/index.ts
@@ -37,6 +37,53 @@ const MockedQuestions: MockedQuestionT = {
     [
       {
         id: 3,
+        other_person_received_value: 100,
+        other_person_offer: 50,
+      },
+      {
+        id: 4,
+        other_person_received_value: 100,
+        other_person_offer: 40,
+      },
+      {
+        id: 5,
+        other_person_received_value: 100,
+        other_person_offer: 30,
+      },
+      {
+        id: 6,
+        other_person_received_value: 100,
+        other_person_offer: 20,
+      },
+      {
+        id: 7,
+        other_person_received_value: 100,
+        other_person_offer: 15,
+      },
+      {
+        id: 8,
+        other_person_received_value: 100,
+        other_person_offer: 10,
+      },
+      {
+        id: 9,
+        other_person_received_value: 100,
+        other_person_offer: 8,
+      },
+      {
+        id: 10,
+        other_person_received_value: 100,
+        other_person_offer: 5,
+      },
+      {
+        id: 11,
+        other_person_received_value: 100,
+        other_person_offer: 3,
+      },
+    ],
+    [
+      {
+        id: 3,
         closest: {
           value: 34,
           days: 0,
@@ -58,11 +105,6 @@ const MockedQuestions: MockedQuestionT = {
           days: 117,
         },
         discount_rate: "0.00016",
-      },
-      {
-        id: 5,
-        other_person_received_value: 50,
-        other_person_offer: 25,
       },
       {
         id: 6,
@@ -89,11 +131,6 @@ const MockedQuestions: MockedQuestionT = {
         discount_rate: "0.00040",
       },
       {
-        id: 8,
-        other_person_received_value: 100,
-        other_person_offer: 50,
-      },
-      {
         id: 9,
         closest: {
           value: 47,
@@ -118,23 +155,6 @@ const MockedQuestions: MockedQuestionT = {
         discount_rate: "0.00040",
       },
       {
-        id: 11,
-        other_person_received_value: 150,
-        other_person_offer: 75,
-      },
-    ],
-    [
-      {
-        id: 12,
-        other_person_received_value: 50,
-        other_person_offer: 20,
-      },
-      {
-        id: 13,
-        other_person_received_value: 100,
-        other_person_offer: 40,
-      },
-      {
         id: 14,
         closest: {
           value: 22,
@@ -145,16 +165,6 @@ const MockedQuestions: MockedQuestionT = {
           days: 136,
         },
         discount_rate: "0.0010",
-      },
-      {
-        id: 15,
-        other_person_received_value: 150,
-        other_person_offer: 60,
-      },
-      {
-        id: 16,
-        other_person_received_value: 50,
-        other_person_offer: 15,
       },
       {
         id: 17,
@@ -169,16 +179,6 @@ const MockedQuestions: MockedQuestionT = {
         discount_rate: "0.0010",
       },
       {
-        id: 18,
-        other_person_received_value: 100,
-        other_person_offer: 30,
-      },
-      {
-        id: 19,
-        other_person_received_value: 150,
-        other_person_offer: 45,
-      },
-      {
         id: 20,
         closest: {
           value: 67,
@@ -189,320 +189,6 @@ const MockedQuestions: MockedQuestionT = {
           days: 119,
         },
         discount_rate: "0.0010",
-      },
-    ],
-    [
-      {
-        id: 21,
-        closest: {
-          value: 25,
-          days: 0,
-        },
-        furthest: {
-          value: 30,
-          days: 80,
-        },
-        discount_rate: "0.0025",
-      },
-      {
-        id: 22,
-        closest: {
-          value: 49,
-          days: 0,
-        },
-        furthest: {
-          value: 60,
-          days: 89,
-        },
-        discount_rate: "0.0025",
-      },
-      {
-        id: 23,
-        other_person_received_value: 50,
-        other_person_offer: 10,
-      },
-      {
-        id: 24,
-        closest: {
-          value: 69,
-          days: 0,
-        },
-        furthest: {
-          value: 85,
-          days: 91,
-        },
-        discount_rate: "0.0025",
-      },
-      {
-        id: 25,
-        closest: {
-          value: 19,
-          days: 0,
-        },
-        furthest: {
-          value: 25,
-          days: 53,
-        },
-        discount_rate: "0.0060",
-      },
-      {
-        id: 26,
-        other_person_received_value: 100,
-        other_person_offer: 20,
-      },
-      {
-        id: 27,
-        closest: {
-          value: 40,
-          days: 0,
-        },
-        furthest: {
-          value: 55,
-          days: 62,
-        },
-        discount_rate: "0.0060",
-      },
-      {
-        id: 28,
-        closest: {
-          value: 55,
-          days: 0,
-        },
-        furthest: {
-          value: 75,
-          days: 61,
-        },
-        discount_rate: "0.0060",
-      },
-      {
-        id: 29,
-        other_person_received_value: 150,
-        other_person_offer: 30,
-      },
-    ],
-    [
-      {
-        id: 30,
-        other_person_received_value: 50,
-        other_person_offer: 7.5,
-      },
-      {
-        id: 31,
-        other_person_received_value: 100,
-        other_person_offer: 15,
-      },
-      {
-        id: 32,
-        closest: {
-          value: 24,
-          days: 0,
-        },
-        furthest: {
-          value: 35,
-          days: 29,
-        },
-        discount_rate: "0.016",
-      },
-      {
-        id: 33,
-        other_person_received_value: 150,
-        other_person_offer: 22.5,
-      },
-      {
-        id: 34,
-        other_person_received_value: 50,
-        other_person_offer: 5,
-      },
-      {
-        id: 35,
-        closest: {
-          value: 34,
-          days: 0,
-        },
-        furthest: {
-          value: 50,
-          days: 30,
-        },
-        discount_rate: "0.016",
-      },
-      {
-        id: 36,
-        other_person_received_value: 100,
-        other_person_offer: 10,
-      },
-      {
-        id: 37,
-        other_person_received_value: 150,
-        other_person_offer: 15,
-      },
-      {
-        id: 38,
-        closest: {
-          value: 54,
-          days: 0,
-        },
-        furthest: {
-          value: 80,
-          days: 30,
-        },
-        discount_rate: "0.016",
-      },
-    ],
-    [
-      {
-        id: 39,
-        closest: {
-          value: 14,
-          days: 0,
-        },
-        furthest: {
-          value: 25,
-          days: 19,
-        },
-        discount_rate: "0.041",
-      },
-      {
-        id: 40,
-        closest: {
-          value: 27,
-          days: 0,
-        },
-        furthest: {
-          value: 50,
-          days: 21,
-        },
-        discount_rate: "0.041",
-      },
-      {
-        id: 41,
-        other_person_received_value: 50,
-        other_person_offer: 4,
-      },
-      {
-        id: 42,
-        closest: {
-          value: 41,
-          days: 0,
-        },
-        furthest: {
-          value: 75,
-          days: 20,
-        },
-        discount_rate: "0.041",
-      },
-      {
-        id: 43,
-        closest: {
-          value: 15,
-          days: 0,
-        },
-        furthest: {
-          value: 35,
-          days: 13,
-        },
-        discount_rate: "0.10",
-      },
-      {
-        id: 44,
-        other_person_received_value: 100,
-        other_person_offer: 8,
-      },
-      {
-        id: 45,
-        closest: {
-          value: 25,
-          days: 0,
-        },
-        furthest: {
-          value: 60,
-          days: 14,
-        },
-        discount_rate: "0.10",
-      },
-      {
-        id: 46,
-        closest: {
-          value: 33,
-          days: 0,
-        },
-        furthest: {
-          value: 80,
-          days: 14,
-        },
-        discount_rate: "0.10",
-      },
-      {
-        id: 47,
-        other_person_received_value: 150,
-        other_person_offer: 12,
-      },
-    ],
-    [
-      {
-        id: 48,
-        other_person_received_value: 50,
-        other_person_offer: 2.5,
-      },
-      {
-        id: 49,
-        other_person_received_value: 100,
-        other_person_offer: 5,
-      },
-      {
-        id: 50,
-        closest: {
-          value: 11,
-          days: 0,
-        },
-        furthest: {
-          value: 30,
-          days: 7,
-        },
-        discount_rate: "0.25",
-      },
-      {
-        id: 51,
-        other_person_received_value: 150,
-        other_person_offer: 7.5,
-      },
-      {
-        id: 52,
-        other_person_received_value: 50,
-        other_person_offer: 1.5,
-      },
-      {
-        id: 53,
-        closest: {
-          value: 20,
-          days: 0,
-        },
-        furthest: {
-          value: 55,
-          days: 7,
-        },
-        discount_rate: "0.25",
-      },
-      {
-        id: 54,
-        other_person_received_value: 100,
-        other_person_offer: 3,
-      },
-      {
-        id: 55,
-        other_person_received_value: 150,
-        other_person_offer: 4.5,
-      },
-      {
-        id: 56,
-        closest: {
-          value: 31,
-          days: 0,
-        },
-        furthest: {
-          value: 85,
-          days: 7,
-        },
-        discount_rate: "0.25",
       },
     ],
   ],
