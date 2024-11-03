a830cddcb7b7904936f64ef2ed241e03fd48d7f1
Erick Oda <erickodac@hotmail.com>
2024-11-03 12:03:30 -0300

    refactor: changed instructions page

--- src/app/components/Pages/Instructions/index.tsx
+++ src/app/components/Pages/Instructions/index.tsx
@@ -20,54 +20,30 @@ const Instructions = () => {
   return (
     <>
       <div className="flex flex-col space-y-1">
-        <Title>Instruções</Title>
+        <Title>Aviso!</Title>
         <Paragraph>
-          Por favor, durante o experimento procure permanecer sentado(a) numa
-          posição cômoda e relaxada, sem distrações.
+          Por favor, durante o experimento utilize um fone de ouvido e mantenha
+          o volume em uma altura confortável, procure permanecer sentado(a) numa
+          posição cômoda e relaxada, sem distrações. 
           <b> Lembre-se: sua participação é muito importante para nós.</b>
-          Você passará pelo treino com 2 questões e em seguida para o experimento
-          com 18 questões semelhantes ao treino, ok?
         </Paragraph>
       </div>
 
       <div className="flex flex-col space-y-1">
         <h4 className="text-lg font-bold">Passo a Passo</h4>
         <Paragraph>
-          <ul>
-            <li>
-              • Clique sobre o botão Iniciar da apresentação. <br />
-            </li>
-
-            <li>
-              • Quando a opções de escolhas destacarem, clique na opção
-              escolhida, após clique em botão Próximo. <br />
-            </li>
-
-            <li>
-              • Em seguida você irá reproduzir a duração total da apresentação e
-              escolha, que você acabou de fazer. <br />
-            </li>
-
-            <li>
-              • Para isso você deve apertar a tecla INÍCIO e deixar o tempo
-              passar. <br />
-            </li>
-
-            <li>
-              • Quando você achar que o tempo que está passando for igual ao do
-              estímulo apresentado, aperte a tecla FIM. <br />
-            </li>
-          </ul>
+          Você passará pelo treino com 2 questões e em seguida para o experimento
+          com 18 questões semelhantes ao treino, ok?
         </Paragraph>
       </div>
 
       <div className="flex flex-col space-y-1">
         <h4 className="text-lg font-bold">Atenção!</h4>
         <Paragraph>
-          É muito importante que você não utilize nenhum recurso para contar a
-          duração do estímulo, como contar, bater os pés, bater as mãos etc.
-          Isso pode interferir nos resultados do estudo. Ao término deste
-          treino, você deverá clicar em Próximo.
+          É muito importante que você não utilize nenhum recurso para
+          mensurar a duração dos procedimentos, como contar, bater os pés,
+          bater as mãos, etc. Ao término deste treino, será sinalizado o início do
+          experimento e você iniciará clicando no botão “Continuar”.
         </Paragraph>
       </div>
 
