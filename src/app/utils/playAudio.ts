f9aea43bf3ba06bc5697f1d8acdacd9c6d1618d6
Erick Oda <erickodac@hotmail.com>
2024-11-18 22:19:11 -0300

    fix: fixed audio duration

--- src/app/utils/playAudio.ts
+++ src/app/utils/playAudio.ts
@@ -1,19 +1,19 @@
-export default function playAudio(hertz: number = 440): void {
-    const audioContext = new window.AudioContext;
-    const oscillator = audioContext.createOscillator();
-    const gainNode = audioContext.createGain();
+export default function playAudio(hertz: number = 160): void {
+  const audioContext = new window.AudioContext;
+  const oscillator = audioContext.createOscillator();
+  const gainNode = audioContext.createGain();
 
-    oscillator.connect(gainNode);
-    gainNode.connect(audioContext.destination);
+  oscillator.connect(gainNode);
+  gainNode.connect(audioContext.destination);
 
-    oscillator.type = 'square'; // Tipo de onda: square, sine, sawtooth, triangle
-    oscillator.frequency.setValueAtTime(hertz, audioContext.currentTime); // Frequência em Hz
+  oscillator.type = 'square'; // Tipo de onda: square, sine, sawtooth, triangle
+  oscillator.frequency.setValueAtTime(hertz, audioContext.currentTime); // Frequência em Hz
 
-    gainNode.gain.setValueAtTime(0.8, audioContext.currentTime); // Volume
-    oscillator.start(audioContext.currentTime);
-    oscillator.stop(audioContext.currentTime + 0.5); // Duração do beep: 1 segundo
+  gainNode.gain.setValueAtTime(0.1, audioContext.currentTime);
+  oscillator.start(audioContext.currentTime);
+  oscillator.stop(audioContext.currentTime + 2); 
 
-    oscillator.onended = () => {
-      audioContext.close();
-    };
+  oscillator.onended = () => {
+    audioContext.close();
+  };
 }
